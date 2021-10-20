4a5b0928e2347c7ab7214ed35aae1585875fa475
Михаил Гусев <gusev-miha2011@mail.ru>
2021-10-20 22:33:48 +1000

    Lab done

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import csv
-from openpyxl import load_workbook
+import openpyxl
 
 
 def unique_words():
@@ -61,14 +61,36 @@ def sort_salary():
         for row in file_reader:
             company_salary[row[0]] = int(row[1])
 
-        sorted_company_salary = dict((sorted(company_salary.items(), key=lambda x: x[1])))
+        sorted_by_names = dict((sorted(company_salary.items(), key=lambda x: x[0])))
+        sorted_by_salary = dict((sorted(sorted_by_names.items(), key=lambda x: x[1])))
 
-        for company in sorted_company_salary:
-            print(f'{company}: {sorted_company_salary[company]}')
+        for company in sorted_by_salary:
+            print(f'{company}: {sorted_by_salary[company]}')
 
 
 def sort_calories():
-    wb = load_workbook('files/trekking1.xlsx')
-    print(wb.sheetnames)
+    book = openpyxl.open('files/trekking1.xlsx', read_only=True)
+    sheet = book.active
+    product_calories = {}
+    for row in range(1, sheet.max_row):
+        if row == 1: continue
+        product_calories[sheet[row][0].value] = int(sheet[row][1].value)
+
+    sorted_by_names = dict((sorted(product_calories.items(), key=lambda x: x[0])))
+    sorted_by_calories = dict((sorted(sorted_by_names.items(), key=lambda x: x[1], reverse=True)))
+
+    for product in sorted_by_calories:
+        print(f'{product}: {sorted_by_calories[product]}')
+
+
+unique_words()
+
+quantity_every_word()
+
+longest_rows()
+
+reverse_text()
+
+sort_salary()
 
 sort_calories()
